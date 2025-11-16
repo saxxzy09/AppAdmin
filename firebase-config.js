ea8f8ee44cc0bd82d007aa57d522fe7611a405bf
saxxzy09 <fr776022@gmail.com>
2025-11-16 17:11:50 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,23 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-analytics.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAmn6QYlRsec2c0v1WSJeS_ryZlCVQHhHE",
+  authDomain: "craked-panel.firebaseapp.com",
+  databaseURL: "https://craked-panel-default-rtdb.firebaseio.com",
+  projectId: "craked-panel",
+  storageBucket: "craked-panel.firebasestorage.app",
+  messagingSenderId: "670353278206",
+  appId: "1:670353278206:web:93c9ced7b309c2417d51af",
+  measurementId: "G-WQTCB8ZWK4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getDatabase(app);
+
+// Export the initialized services
+export { app, analytics, db };
